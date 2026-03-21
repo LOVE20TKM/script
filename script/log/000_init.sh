badc2ab729dfa83f162dba4d17c378088eec5e6d
BigPolarBear <hanxincade@gmail.com>
2026-03-22 05:28:04 +0800

    Add LIFE20 address exports to init and log config

--- script/log/000_init.sh
+++ script/log/000_init.sh
@@ -27,15 +27,28 @@ normalize_address() {
     else
         read address
     fi
+
+    if [ -z "$address" ]; then
+        return 0
+    fi
     
     # 去掉 0x 前缀
     address=${address#0x}
+
+    if [ ${#address} -lt 40 ]; then
+        return 0
+    fi
     
     # 取最后40个字符（20字节地址）
     address=${address: -40}
     
     # 重新添加 0x 前缀
     echo "0x$address"
+}
+
+is_nonzero_address() {
+    local address=$1
+    [ -n "$address" ] && [ "$address" != "0x0000000000000000000000000000000000000000" ]
 }
 
 cast_call() {
@@ -45,26 +58,92 @@ cast_call() {
     cast call "$address" "$function_signature" "$@" --rpc-url "$RPC_URL"
 }
 
+init_token_address_set() {
+    local token_var_name=$1
+    local token_address=$2
+    local prefix=$3
+
+    if ! is_nonzero_address "$token_address"; then
+        return 0
+    fi
+
+    export "$token_var_name=$token_address"
+
+    local st_var_name
+    local sl_var_name
+    if [ -n "$prefix" ]; then
+        st_var_name="${prefix}StTokenAddress"
+        sl_var_name="${prefix}SlTokenAddress"
+    else
+        st_var_name="stTokenAddress"
+        sl_var_name="slTokenAddress"
+    fi
+
+    local st_address
+    st_address=$(cast_call "$token_address" "stAddress()(address)" 2>/dev/null | normalize_address)
+    if is_nonzero_address "$st_address"; then
+        export "$st_var_name=$st_address"
+    fi
+
+    local sl_address
+    sl_address=$(cast_call "$token_address" "slAddress()(address)" 2>/dev/null | normalize_address)
+    if is_nonzero_address "$sl_address"; then
+        export "$sl_var_name=$sl_address"
+    fi
+}
+
+init_extension_addresses() {
+    local token_address=$1
+    local prefix=$2
+    shift 2
+
+    if ! is_nonzero_address "$token_address"; then
+        return 0
+    fi
+
+    local actionId
+    for actionId in "$@"; do
+        local ext_var_name
+        if [ -n "$prefix" ]; then
+            ext_var_name="${prefix}Ext${actionId}Address"
+        else
+            ext_var_name="ext${actionId}Address"
+        fi
+        local ext_address
+        ext_address=$(cast_call "$centerAddress" "extension(address,uint256)(address)" "$token_address" "$actionId" 2>/dev/null | normalize_address)
+        if is_nonzero_address "$ext_address"; then
+            export "$ext_var_name=$ext_address"
+        fi
+    done
+}
+
+init_pair_address() {
+    local token0_address=$1
+    local token1_address=$2
+    local pair_var_name=$3
+
+    if ! is_nonzero_address "$token0_address" || ! is_nonzero_address "$token1_address"; then
+        return 0
+    fi
+
+    local pair_address
+    pair_address=$(cast_call "$uniswapV2FactoryAddress" "getPair(address,address)(address)" "$token0_address" "$token1_address" 2>/dev/null | normalize_address)
+    if is_nonzero_address "$pair_address"; then
+        export "$pair_var_name=$pair_address"
+    fi
+}
+
 export tokenAddress=$firstTokenAddress
-export stTokenAddress=$(cast_call $tokenAddress "stAddress()(address)" 2>/dev/null | normalize_address)
-export slTokenAddress=$(cast_call $tokenAddress "slAddress()(address)" 2>/dev/null | normalize_address)
+init_token_address_set "tokenAddress" "$tokenAddress" ""
+init_extension_addresses "$tokenAddress" "" 24 25 26 27 28 29
+init_pair_address "$tokenAddress" "$rootParentTokenAddress" "love20Tkm20PairAddress"
+init_pair_address "$tokenAddress" "$tusdtAddress" "love20TusdtPairAddress"
 
-# Extension actions 24-28 (24: lpFactory, 25-27: groupActionFactory, 28: groupServiceFactory)
-for actionId in 24 25 26 27 28 29; do
-  addr=$(cast_call $centerAddress "extension(address,uint256)(address)" $firstTokenAddress $actionId 2>/dev/null | normalize_address)
-  if [ -n "$addr" ] && [ "$addr" != "0x0000000000000000000000000000000000000000" ]; then
-    export ext${actionId}Address=$addr
-  fi
-done
-
-# Uniswap V2 pairs: LOVE20-TKM20, LOVE20-TUSDT
-love20Tkm20Pair=$(cast_call $uniswapV2FactoryAddress "getPair(address,address)(address)" $firstTokenAddress $rootParentTokenAddress 2>/dev/null | normalize_address)
-if [ -n "$love20Tkm20Pair" ] && [ "$love20Tkm20Pair" != "0x0000000000000000000000000000000000000000" ]; then
-  export love20Tkm20PairAddress=$love20Tkm20Pair
-fi
-love20TusdtPair=$(cast_call $uniswapV2FactoryAddress "getPair(address,address)(address)" $firstTokenAddress $tusdtAddress 2>/dev/null | normalize_address)
-if [ -n "$love20TusdtPair" ] && [ "$love20TusdtPair" != "0x0000000000000000000000000000000000000000" ]; then
-  export love20TusdtPairAddress=$love20TusdtPair
+life20Address=$(cast_call "$launchAddress" "tokenAddressBySymbol(string)(address)" "LIFE20" 2>/dev/null | normalize_address)
+if is_nonzero_address "$life20Address"; then
+  init_token_address_set "life20Address" "$life20Address" "life20"
+  init_extension_addresses "$life20Address" "life20" 0 1 2 3 4 5 6
+  init_pair_address "$firstTokenAddress" "$life20Address" "love20Life20PairAddress"
 fi
 
 export maxBlocksPerRequest=50000  # Large chunks for Python processor
